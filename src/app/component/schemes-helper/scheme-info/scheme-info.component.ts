8d441065becb0e46ed135ff52a425043d5b5d3e3
AnubhavJain2006 <Anubhavjainjaak@gmail.com>
2021-11-06 00:47:11 +0530

    scheme-info added

--- src/app/component/schemes-helper/scheme-info/scheme-info.component.ts
+++ src/app/component/schemes-helper/scheme-info/scheme-info.component.ts
@@ -1,3 +1,6 @@
+import { Scheme } from './../../../interface/scheme';
+import { SchemeService } from './../../../service/scheme.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheme-info.component.css']
 })
 export class SchemeInfoComponent implements OnInit {
-
-  constructor() { }
+  id!: string;
+  schemeDetails!: Scheme;
+  dataLoadingStatus: boolean = true;
+  constructor(private activateRoute: ActivatedRoute, private schemeService: SchemeService) { }
 
   ngOnInit(): void {
+    this.id = this.activateRoute.snapshot.params.id;
+    this.schemeService.getSchemeById(this.id).subscribe(resp => {
+      this.schemeDetails = resp;
+      this.dataLoadingStatus = false;
+    }, error => {
+      console.log(error);
+    }
+    )
   }
 
 }
